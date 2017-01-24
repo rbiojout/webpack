bbc6ed1227fa851bf4282c865422e3aee2991c1d
R. Biojout <rbiojout@gmail.com>
2017-01-24 18:04:28 +0100

    Initial commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,11 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.public_file_server.enabled = true
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
 
   # Compress JavaScripts and CSS.
   # config.assets.js_compressor = :uglifier
@@ -25,6 +30,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+
 
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
